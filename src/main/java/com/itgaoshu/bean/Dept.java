75168e64d0501d94ff47907e8e94e02ce3823db9
Administrator <caochuanxia@163.com>
2020-04-10 11:20:31 +0800

    修改部门实体类:增加属性

--- src/main/java/com/itgaoshu/bean/Dept.java
+++ src/main/java/com/itgaoshu/bean/Dept.java
@@ -7,4 +7,22 @@ package com.itgaoshu.bean;
  * @date 2020/4/10 0010 11:15
  */
 public class Dept {
+    private int deptno;
+    private String dname;
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
 }
